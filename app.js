cf0e49d6b5c175cd42a73bdd20dfae5201e3835b
goat-dev-gaurav <ssood463@gmail.com>
2024-06-06 16:41:28 +0530

    initial stage 2

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const app = express();
 
+app.set("view engine" , "ejs")
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname , "public")))
 app.use(cookieParser());
-
 
 app.use("/user" , usersRouter)
 app.use("/product" , productsRouter)
